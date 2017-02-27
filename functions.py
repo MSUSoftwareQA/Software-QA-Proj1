35967c614d8904e6b16fb0aabb890d5a7cdf32df
Alex Cote <alx929@gmail.com>
2017-02-26 21:39:45 -0600

    Corrected the bug in email verifier that didn't properly check forr the . char. Added in error handling for the other three functions to take into account when incorrect input is given. Removed testing for these exceptions as well since they will no longer be thrown. The tests now just look for the error value to be returned to the test.

--- functions.py
+++ functions.py
@@ -2,13 +2,22 @@ import math
 import re
 
 def BMICalc(usrWeight, usrHeight):
-	return ((usrWeight / (usrHeight**2)) * 703)
+	try:
+		return ((float(usrWeight) / (float(usrHeight)**2)) * 703)
+	except:
+		return 0
 
 def retCalc(usrAge, usrSal, usrSav, savGoal):
-	return int(usrAge + math.floor(savGoal / (usrSal * (usrSav / 100))))
+	try:
+		return int(usrAge + math.floor(savGoal / (usrSal * (usrSav / 100))))
+	except:
+		return 0
 
 def distCalc(x1, x2, y1, y2):
-	return (math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2)))
+	try:
+		return (math.sqrt(((x2 - x1)**2) + ((y2 - y1)**2)))
+	except:
+		return 0
 
 def emailVerify(usrEmail):
-	return bool(re.match("\w\w*@\w\w*.[a-zA-Z]{3}$", usrEmail))
+	return bool(re.match("\w\w*@\w\w*\.[a-zA-Z]{3}$", str(usrEmail)))
--- test.py
+++ test.py
@@ -2,39 +2,34 @@ import pytest
 import functions
 
 def test_BMI():
-	assert functions.BMICalc(100, 60) == 19.527777777777775
-	assert functions.BMICalc(200, 90) == 17.358024691358025
-	assert functions.BMICalc(12, 23) == 15.947069943289225
-	with pytest.raises(TypeError):
-		assert functions.BMICalc('a', 23) != 20
-	with pytest.raises(ValueError)
-		assert functions.BMICalc("a","b")
+	assert functions.BMICalc(100, 60) == 19.527777777777775 #Alex
+	assert functions.BMICalc(200, 90) == 17.358024691358025 #Alex
+	assert functions.BMICalc(12, 23) == 15.947069943289225 #Alex
+	assert functions.BMICalc('a', 23) != 20 #Pat
+	assert functions.BMICalc("a","b") == 0 #Ginny
 
 def test_Ret():
-	assert functions.retCalc(20, 40000, 30, 50000) == 24
-	assert functions.retCalc(5, 10, 100, 500) == 55
-	assert functions.retCalc(99, 1000, 2, 500) == 124
-	with pytest.raises(TypeError):
-		assert functions.retCalc('a', 23,"g", 50) != 20
-	assert functions.retCalc(-0, 45, -45, 0) == 0
-	with pytest.raises(ValueError)
-		assert functions.retCalc("a", "food", 5, "hello")
+	assert functions.retCalc(20, 40000, 30, 50000) == 24 #Alex
+	assert functions.retCalc(5, 10, 100, 500) == 55 #Alex
+	assert functions.retCalc(99, 1000, 2, 500) == 124 #Alex
+	assert functions.retCalc('a', 23,"g", 50) != 20 #Pat
+	assert functions.retCalc(-0, 45, -45, 0) == 0 #Pat
+	assert functions.retCalc("a", "food", 5, "hello") == 0 #Ginny
 
 def test_Dist():
-	assert functions.distCalc(5, 5, 10, 5) == 5
-	assert functions.distCalc(1, 50, 120, 3) == 126.84636376341263
-	assert functions.distCalc(9999, 12, 3, 45920) == 46990.54221862097
-	with pytest.raises(TypeError):
-		assert functions.distCalc('a', 23, 200,30) != 20
-	assert functions.distCalc(-11, -100, 5, 90) == 190.67249408344142
+	assert functions.distCalc(5, 5, 10, 5) == 5 #Alex
+	assert functions.distCalc(1, 50, 120, 3) == 126.84636376341263 #Alex
+	assert functions.distCalc(9999, 12, 3, 45920) == 46990.54221862097 #Alex
+	assert functions.distCalc('a', 23, 200,30) != 20 #Pat
+	assert functions.distCalc(-11, -100, 5, 90) == 123.0690862889621 #Ginny
 
 def test_Email():
-	assert functions.emailVerify("alc552@msstate.edu") == True
-	assert functions.emailVerify("$Asd%!@msstate.edu") == False
-	assert functions.emailVerify("@.edu") == False
-	assert functions.emailVerify("alc552.msstate@edu") == False
-	assert functions.emailVerify("alc552.msstateedu") == False
-	assert functions.emailVerify("alc55@edu.msstate") == False
-	assert functions.emailVerify("dddd@dddd") == False
+	assert functions.emailVerify("alc552@msstate.edu") == True #Alex
+	assert functions.emailVerify("$Asd%!@msstate.edu") == False #Alex
+	assert functions.emailVerify("@.edu") == False #Alex
+	assert functions.emailVerify("alc552.msstate@edu") == False #Ginny
+	assert functions.emailVerify("alc552.msstateedu") == False #Damian
+	assert functions.emailVerify("alc55@edu.msstate") == False #Damian
+	assert functions.emailVerify("dddd@dddd") == False #Pat
 	assert functions.emailVerify("dddddddddddd@ddddddddd") == False
-	assert functions.emailVerify(11) == False
+	assert functions.emailVerify(11) == False #Ginny
